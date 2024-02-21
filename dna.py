55476b97dbc7b5eafade94102f597d48579977a7
Elham parhizgar <160337160+elhamparhizgar2002@users.noreply.github.com>
2024-02-21 10:51:39 +0100

    Update dna.py

--- dna.py
+++ dna.py
@@ -1,16 +1,2 @@
-def dna(s):
-    count_A=s.count("A")
-    count_C = s.count("C")
-    count_G = s.count("G")
-    count_T = s.count("T")
-    return str(count_A)+" "+str(count_C)+" "+str(count_G)+" "+str(count_T)
-
-print(dna("AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC"))
-
-'''
-count_A=s.count("A")
-count_C = s.count("C")
-count_G = s.count("G")
-count_T = s.count("T")
-print(str(count_A)+" "+str(count_C)+" "+str(count_G)+" "+str(count_T))
-'''
+s='AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC'
+print(s.count('A'), s.count('C'), s.count('G'), s.count('T'))
